16ec5350cf12c4bc7df73d5050e067b8d8be09a5
iratnayak <iratnayake6@gmail.com>
2025-12-19 22:38:24 +0530

    Added Closure example

--- closure.js
+++ closure.js
@@ -1,25 +1,20 @@
 function createCounter() {
-    let count = 0; // This variable is enclosed in the closure
+  let count = 0;
 
-    return function() {
-        count++; // Inner function remembers 'count'
-        console.log("Count is:", count);
-    };
+  return function() {
+    count++;
+    console.log("Count is: ", count);
+    
+  };
 }
 
 const myCounter = createCounter();
 
-console.log("1. First Call:");
-myCounter(); // Output: 1
+console.log(" 1. First Call:");
+myCounter();
 
-console.log("2. Second Call:");
-myCounter(); // Output: 2
+console.log(" 2. Second Call:");
+myCounter();
 
-console.log("3. Third Call:");
-myCounter(); // Output: 3
-
-/* Interview Explanation:
-  Even though 'createCounter' has finished executing, 
-  the inner function still has access to the 'count' variable.
-  This memory preservation is called a 'Closure'.
-*/
+console.log(" 3. Third Call:");
+myCounter();
